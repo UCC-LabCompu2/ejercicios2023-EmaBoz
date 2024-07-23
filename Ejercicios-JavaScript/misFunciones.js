c5c5ed1896e0f5a3e048bdf2dc9d85ff994e05bb
EmaBoz <166394476+EmaBoz@users.noreply.github.com>
2024-07-23 10:54:43 -0300

    Update misFunciones.js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,7 +1,40 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * conversor de unidades
+ * @method cambiar unidades
+ * @param {string}id-el id de los inputs 
+ * @param {number}valor-el valor de los inputs
  */
+let convertirUnidades = (id, value) => {
+    let metro,pie,yarda,pulgada;
+    if (isNaN(value)) {
+        alert("Ingrese numeros unicamente");
+        metro = "";
+        pulgada = "";
+        pie = "";
+        yarda = "";
+    } else if (id === "metro") {
+        metro = value;
+        pulgada = value * 39.3701;
+        pie = value * 3.28084;
+        yarda = value * 1.09361;
+    } else if (id === "pulgada") {
+        pulgada = value;
+        metro = value * 0.0254;
+        pie = value * 0.0833333;
+        yarda = value * 0.0277778;
+    } else if (id === "pie") {
+        pie = value;
+        metro = value * 0.3048;
+        pulgada = value * 12;
+        yarda = value * 0.333333;
+    } else if (id === "yarda") {
+        yarda = value;
+        metro = value * 0.9144;
+        pulgada = value * 36;
+        pie = value * 3;
+    }
+    document.getElementById("metro").value = metro;
+    document.getElementById("pulgada").value = pulgada;
+    document.getElementById("pie").value = pie;
+    document.getElementById("yarda").value = yarda;
+}
